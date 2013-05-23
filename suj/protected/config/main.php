5e70bf99549e2feba2cb19794f8bc1229495d11c
Andrew <foryoung89@gmail.com>
2013-05-23 19:04:32 +0800

    main

--- suj/protected/config/main.php
+++ suj/protected/config/main.php
@@ -71,10 +71,10 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=startupjobs',
+			'connectionString' => 'mysql:host=localhost;dbname=mydb',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => '123456',
 			'charset' => 'utf8',
 		),
 		
